9b9feee02dc4c23430e48cc9eefe9cbe3a14c271
Hector <hectorlarez1@gmail.com>
2017-02-26 22:55:25 -0430

    Se arreglo typo en consulta del metodo update, reemplazo NO_TEXIL por NO_TEXTIL

--- src/app/modelo/dao/MateriaPrimaNoTextilDao.java
+++ src/app/modelo/dao/MateriaPrimaNoTextilDao.java
@@ -186,21 +186,4 @@ public class MateriaPrimaNoTextilDao implements IDataDao<MateriaPrimaNoTextilVo>
         
         return (deletedRecords > 0);        
     }
-    
-    public static void main(String[] args){
-        MySqlDbConnection conexion = new MySqlDbConnection();
-        MateriaPrimaNoTextilDao mptd = new MateriaPrimaNoTextilDao();
-        
-        // Insertar
-        HashMap options = new HashMap();
-        MateriaPrimaNoTextilVo mptv = new MateriaPrimaNoTextilVo();
-        mptd.insertRecord(conexion, mptv, options);
-        // Listar
-        List<MateriaPrimaNoTextilVo> lista = mptd.getList(conexion, options);
-        // Actualizar
-        mptv.setPeso(1);
-        mptd.updateRecord(conexion, mptv, options);
-        // Eliminar
-        mptd.deleteRecord(conexion, mptv, options);
-    }
 }
